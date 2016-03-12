f4e9176dbe1536952d746e9a9fcfc2f1ca319fce
htha9587 <harrison.thacker69@gmail.com>
2016-03-11 19:24:27 -0700

    Version 1.1
    Added Dolan Icon Functionality.

--- src/FormativeView/FormativeView.java
+++ src/FormativeView/FormativeView.java
@@ -1,5 +1,6 @@
 package FormativeView;
 
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 /**
  * Version 1.0
@@ -9,26 +10,36 @@ import javax.swing.JOptionPane;
 
 public class FormativeView 
 {
-
+	private String windowMessage;
+	private ImageIcon formativeIcon;
+	
+	
 	/**
 	 * Gets answer and then displays Message using JOptionPane library.
 	 * @param input
 	 * @return
 	 */
+	
+	public FormativeView()
+	{
+		windowMessage = "Formative Message.";
+		formativeIcon = new ImageIcon(this.getClass().getResource("/images/Dolan.jpg"));
+		
+	}
+	
 	public String getAnswer(String input)
 	{
+		
 		String answer = "";
-		answer = JOptionPane.showInputDialog(null, input);
+		answer = JOptionPane.showInputDialog(null, input, windowMessage, JOptionPane.INFORMATION_MESSAGE, formativeIcon, null, "Type here please!").toString();
 		return answer;
 	}
 	
 	public void displayMessage(String input)
 	{
-		JOptionPane.showMessageDialog(null, input);
+		JOptionPane.showMessageDialog(null, input, windowMessage, JOptionPane.PLAIN_MESSAGE, formativeIcon);
 	}
 
- 
-	
 	
 	
 }
